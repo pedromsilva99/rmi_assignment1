08c29f2c09c5949e29b3cb94f82b765e36939935
pedrog-8 <pedrog8@ua.pt>
2021-11-16 15:38:33 +0000

    Initial phase Astar

--- cibertools-v2.2.7.rmi/pClient/c2_pg.py
+++ cibertools-v2.2.7.rmi/pClient/c2_pg.py
@@ -56,6 +56,8 @@ class MyRob(CRobLinkAngs):
 
 
 
+
+
         self.go_left = False
         self.go_right = False
         self.go_back = False
@@ -112,15 +114,22 @@ class MyRob(CRobLinkAngs):
 
         if self.do_astar:
             print('ENTROU NO DO ASTAR')
-            start = (26 - self.pos[1], self.pos[0])
-            end = self.squares_to_visit[0]
-            # end = (13, 29)
-            maze = self.maze
-            print('START: ' + str(start))
-            print('END: ' + str(end))
-            print('MAZE: ' + str(maze))
-            path = astar(maze, start, end)
-            print('Caminho do labirinto: ' + str(path))
+
+            a = AStar()
+            walls = [(0, 5), (1, 0), (1, 1), (1, 5), (2, 3),
+                     (3, 5), (4, 1), (4, 4), (2, 2), (4, 2), (4, 3)]
+            a.init_grid(6, 6, walls, (0, 0), (5, 5))
+            path = a.solve()
+            print(path)
+            # start = (26 - self.pos[1], self.pos[0])
+            # end = self.squares_to_visit[0]
+            # # end = (13, 29)
+            # maze = self.maze
+            # print('START: ' + str(start))
+            # print('END: ' + str(end))
+            # print('MAZE: ' + str(maze))
+            # path = astar(maze, start, end)
+            # print('Caminho do labirinto: ' + str(path))
             exit()
 
         # if self.do_astar:
@@ -434,7 +443,7 @@ class MyRob(CRobLinkAngs):
                 self.previous_pos=26-self.pos[1],self.pos[0]
                 self.flag = 1
                 self.previous += 1
-                if self.previous == 5:
+                if self.previous == 2:
                     self.do_astar = True
                     return
             if self.previous_pos!=(26-self.pos[1],self.pos[0]):
@@ -701,102 +710,142 @@ class MyRob(CRobLinkAngs):
         else:
             pass
 
-class Node():
-    """A node class for A* Pathfinding"""
-
-    def __init__(self, parent=None, position=None):
-        self.parent = parent
-        self.position = position
-
+class Cell(object):
+    def __init__(self, x, y, reachable):
+        """Initialize new cell.
+        @param reachable is cell reachable? not a wall?
+        @param x cell x coordinate
+        @param y cell y coordinate
+        @param g cost to move from the starting cell to this cell.
+        @param h estimation of the cost to move from this cell
+                 to the ending cell.
+        @param f f = g + h
+        """
+        self.reachable = reachable
+        self.x = x
+        self.y = y
+        self.parent = None
         self.g = 0
         self.h = 0
         self.f = 0
 
-    def __eq__(self, other):
-        return self.position == other.position
+    def __lt__(self, other):
+        return self.f < other.f
 
 
-def astar(maze, start, end):
-    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
+class AStar(object):
+    def __init__(self):
+        # open list
+        self.opened = []
+        heapq.heapify(self.opened)
+        # visited cells list
+        self.closed = set()
+        # grid cells
+        self.cells = []
+        self.grid_height = None
+        self.grid_width = None
 
-    # Create start and end node
-    start_node = Node(None, start)
-    start_node.g = start_node.h = start_node.f = 0
-    end_node = Node(None, end)
-    end_node.g = end_node.h = end_node.f = 0
+    def init_grid(self, width, height, walls, start, end):
+        """Prepare grid cells, walls.
+        @param width grid's width.
+        @param height grid's height.
+        @param walls list of wall x,y tuples.
+        @param start grid starting point x,y tuple.
+        @param end grid ending point x,y tuple.
+        """
+        self.grid_height = height
+        self.grid_width = width
+        for x in range(self.grid_width):
+            for y in range(self.grid_height):
+                if (x, y) in walls:
+                    reachable = False
+                else:
+                    reachable = True
+                self.cells.append(Cell(x, y, reachable))
+        self.start = self.get_cell(*start)
+        self.end = self.get_cell(*end)
 
-    # Initialize both open and closed list
-    open_list = []
-    closed_list = []
+    def get_heuristic(self, cell):
+        """Compute the heuristic value H for a cell.
+        Distance between this cell and the ending cell multiply by 10.
+        @returns heuristic value H
+        """
+        return 10 * (abs(cell.x - self.end.x) + abs(cell.y - self.end.y))
 
-    # Add the start node
-    open_list.append(start_node)
+    def get_cell(self, x, y):
+        """Returns a cell from the cells list.
+        @param x cell x coordinate
+        @param y cell y coordinate
+        @returns cell
+        """
+        return self.cells[x * self.grid_height + y]
 
-    # Loop until you find the end
-    while len(open_list) > 0:
+    def get_adjacent_cells(self, cell):
+        """Returns adjacent cells to a cell.
+        Clockwise starting from the one on the right.
+        @param cell get adjacent cells for this cell
+        @returns adjacent cells list.
+        """
+        cells = []
+        if cell.x < self.grid_width-1:
+            cells.append(self.get_cell(cell.x+1, cell.y))
+        if cell.y > 0:
+            cells.append(self.get_cell(cell.x, cell.y-1))
+        if cell.x > 0:
+            cells.append(self.get_cell(cell.x-1, cell.y))
+        if cell.y < self.grid_height-1:
+            cells.append(self.get_cell(cell.x, cell.y+1))
+        return cells
 
-        # Get the current node
-        current_node = open_list[0]
-        current_index = 0
-        for index, item in enumerate(open_list):
-            if item.f < current_node.f:
-                current_node = item
-                current_index = index
+    def get_path(self):
+        cell = self.end
+        path = [(cell.x, cell.y)]
+        while cell.parent is not self.start:
+            cell = cell.parent
+            path.append((cell.x, cell.y))
 
-        # Pop current off open list, add to closed list
-        open_list.pop(current_index)
-        closed_list.append(current_node)
+        path.append((self.start.x, self.start.y))
+        path.reverse()
+        return path
 
-        # Found the goal
-        if current_node == end_node:
-            path = []
-            current = current_node
-            while current is not None:
-                path.append(current.position)
-                current = current.parent
-            return path[::-1] # Return reversed path
+    def update_cell(self, adj, cell):
+        """Update adjacent cell.
+        @param adj adjacent cell to current cell
+        @param cell current cell being processed
+        """
+        adj.g = cell.g + 10
+        adj.h = self.get_heuristic(adj)
+        adj.parent = cell
+        adj.f = adj.h + adj.g
 
-        # Generate children
-        children = []
-        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]: # Adjacent squares
-
-            # Get node position
-            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
-
-            # Make sure within range
-            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
-                continue
-
-            # Make sure walkable terrain
-            if maze[node_position[0]][node_position[1]] != 0:
-                continue
-
-            # Create new node
-            new_node = Node(current_node, node_position)
-
-            # Append
-            children.append(new_node)
-
-        # Loop through children
-        for child in children:
-
-            # Child is on the closed list
-            for closed_child in closed_list:
-                if child == closed_child:
-                    continue
-
-            # Create the f, g, and h values
-            child.g = current_node.g + 1
-            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
-            child.f = child.g + child.h
-
-            # Child is already in the open list
-            for open_node in open_list:
-                if child == open_node and child.g > open_node.g:
-                    continue
-
-            # Add the child to the open list
-            open_list.append(child)
+    def solve(self):
+        """Solve maze, find path to ending cell.
+        @returns path or None if not found.
+        """
+        # add starting cell to open heap queue
+        heapq.heappush(self.opened, (self.start.f, self.start))
+        while len(self.opened):
+            # pop cell from heap queue
+            f, cell = heapq.heappop(self.opened)
+            # add cell to closed list so we don't process it twice
+            self.closed.add(cell)
+            # if ending cell, return found path
+            if cell is self.end:
+                return self.get_path()
+            # get adjacent cells for cell
+            adj_cells = self.get_adjacent_cells(cell)
+            for adj_cell in adj_cells:
+                if adj_cell.reachable and adj_cell not in self.closed:
+                    if (adj_cell.f, adj_cell) in self.opened:
+                        # if adj cell in open list, check if current path is
+                        # better than the one previously found
+                        # for this adj cell.
+                        if adj_cell.g > cell.g + 10:
+                            self.update_cell(adj_cell, cell)
+                    else:
+                        self.update_cell(adj_cell, cell)
+                        # add adj cell to open list
+                        heapq.heappush(self.opened, (adj_cell.f, adj_cell))
 
 
 class Map():
